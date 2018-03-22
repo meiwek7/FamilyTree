abe1cf5fa81693a1754697b807fe1ba7c44d4d4b
Meiwek7 <meiwek7@mail.ru>
2018-03-22 15:24:07 +0200

    Добавил класс User, IAuth, DLLky
    Добавил класс User, IAuth, DLLky

---
+++ BasicLib/IAuth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace BasicLib
+{
+    [ServiceContract]
+    public interface IAuth
+    {
+        [OperationContract]
+        bool Auth(User user);
+        [OperationContract]
+        User Initialize();
+    }
+}
