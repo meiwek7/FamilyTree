d98996dd120626601004f395dffe9c4855a6a2af
Meiwek7 <meiwek7@mail.ru>
2018-03-28 21:49:00 +0300

    1
    1

--- Client/Infrastructure/ConLogic.cs
+++ Client/Infrastructure/ConLogic.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
 using BasicLib;
-using Client.ServiceAuthReference;
+using Client.AuthServiceRef;
 
 namespace Client.Infrastructure
 {
